0465fbb13a5293ba7d60d0c95221dd72941ad766
Dhanesh Neelamana <dhanesh.n.m19@gmail.com>
2013-02-12 22:21:57 -0500

    wrote algorithm for game loop

---
+++ JS/gameloop.js
@@ -0,0 +1,11 @@
+function gameloop(argument) {
+	//1.render UI on canvass
+	//2.Wait for user to hit "go"
+	//3.Pass user program to execute module,ie call execute module
+	//4.execut module runs the simulation and returns result
+	//5.Incvoke AI module and pass the result
+	//6.AI module computes all possible paths and computes the user score.	
+	//7.If score is above 8,player repeat game loop for level2
+	//8.if score less than at,wait player to hit "replay".
+	//9.on replay,repeat gameloop
+}
