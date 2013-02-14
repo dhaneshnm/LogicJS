edf1eba01cf9d9e61c7f5dfcf0714a7b24a32376
Dhanesh Neelamana <dhanesh.n.m19@gmail.com>
2013-02-13 21:59:38 -0500

    while loop to process till the end

--- JS/paths.js
+++ JS/paths.js
@@ -10,67 +10,75 @@ function paths(map,startandend,program,result) {
 	var get_H = function get_H(map,temp_node){
 		return 0;
 	}
+	var findNextNode = function findNextNode(openlist){
+		return openlist[0];//wrong logic,but will change it soon
+	}
 	var openlist = [];
 	var closedlist =[];
 	var start_node = new node(0,0);
 	start_node.row_number = startandend.start/10;
 	start_node.column_number = startandend.start%10;
 	openlist.push(start_node);
-	if(map[start_node.row_number][start_node.column_number+1] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);
-	}
-	if(map[start_node.row_number][start_node.column_number-1] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);	
-	}
-	if(map[start_node.row_number+1][start_node.column_number+1] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);
-	}
-	if(map[start_node.row_number+1][start_node.column_number] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);
-	}
-	if(map[start_node.row_number+1][start_node.column_number-1] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);
-	}
-	if(map[start_node.row_number-1][start_node.column_number+1] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);
-	}
-	if(map[start_node.row_number-1][start_node.column_number] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);
-	}
-	if(map[start_node.row_number-1][start_node.column_number-1] === 0){
-		var temp_node = new node(start_node.row_number,start_node.column_number+1);
-		temp_node.G = start_node.G + 10;
-		temp_node.H = get_H(map,temp_node);
-		temp_node.parent = start_node;
-		openlist.push(temp_node);
+	while(openlist.length >0){
+		if(map[start_node.row_number][start_node.column_number+1] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);
+		}
+		if(map[start_node.row_number][start_node.column_number-1] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);	
+		}
+		if(map[start_node.row_number+1][start_node.column_number+1] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);
+		}
+		if(map[start_node.row_number+1][start_node.column_number] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);
+		}
+		if(map[start_node.row_number+1][start_node.column_number-1] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);
+		}
+		if(map[start_node.row_number-1][start_node.column_number+1] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);
+		}
+		if(map[start_node.row_number-1][start_node.column_number] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);
+		}
+		if(map[start_node.row_number-1][start_node.column_number-1] === 0){
+			var temp_node = new node(start_node.row_number,start_node.column_number+1);
+			temp_node.G = start_node.G + 10;
+			temp_node.H = get_H(map,temp_node);
+			temp_node.parent = start_node;
+			openlist.push(temp_node);
+		}
+		openlist.remove(start_node);
+		closedlist.push(start_node);
+		start_node = findNextNode(openlist);
 	}
 
 }
