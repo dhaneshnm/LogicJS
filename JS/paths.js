6dde813cf721df7640cecb9c0705c49ee7e491b2
Dhanesh Neelamana <dhanesh.n.m19@gmail.com>
2013-02-13 21:50:23 -0500

    structure for inspecting current node

--- JS/paths.js
+++ JS/paths.js
@@ -1,12 +1,47 @@
 function paths(map,startandend,program,result) {
 	var numberofinstructions = program.length;
-	var node = function node(){
+	var node = function node(row_number,column_number){
 		var parent = null;
 		var row_number = 0;
 		var column_number = 0;
 		var G = 0;
 		var H = 0;
 	}
+	var get_H = function get_H(map,temp_node){
+		return 0;
+	}
 	var openlist = [];
 	var closedlist =[];
+	var start_node = new node(0,0);
+	start_node.row_number = startandend.start/10;
+	start_node.column_number = startandend.start%10;
+	openlist.push(start_node);
+	if(map[start_node.row_number][start_node.column_number+1] === 0){
+		var temp_node = new node(start_node.row_number,start_node.column_number+1);
+		temp_node.G = start_node.G + 10;
+		temp_node.H = get_H(map,temp_node);
+		openlist.push(temp_node);
+	}
+	if(map[start_node.row_number][start_node.column_number-1] === 0){
+		
+	}
+	if(map[start_node.row_number+1][start_node.column_number+1] === 0){
+		
+	}
+	if(map[start_node.row_number+1][start_node.column_number] === 0){
+		
+	}
+	if(map[start_node.row_number+1][start_node.column_number-1] === 0){
+		
+	}
+	if(map[start_node.row_number-1][start_node.column_number+1] === 0){
+		
+	}
+	if(map[start_node.row_number-1][start_node.column_number] === 0){
+		
+	}
+	if(map[start_node.row_number-1][start_node.column_number-1] === 0){
+		
+	}
+
 }
