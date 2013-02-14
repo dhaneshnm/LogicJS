8a5f7377137dcbfb8b3c168e0f62faed9208c57a
Dhanesh Neelamana <dhanesh.n.m19@gmail.com>
2013-02-13 21:37:27 -0500

    node function created

--- JS/paths.js
+++ JS/paths.js
@@ -1,3 +1,12 @@
-function paths(map,startandend,program) {
-	// body...
+function paths(map,startandend,program,result) {
+	var numberofinstructions = program.length;
+	var node = function node(){
+		var parent = null;
+		var row_number = 0;
+		var column_number = 0;
+		var G = 0;
+		var H = 0;
+	}
+	var openlist = [];
+	var closedlist =[];
 }
