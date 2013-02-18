60806cc2c205056280c972ebb9cf086f8dd3d05b
Dhanesh Neelamana <dhanesh.n.m19@gmail.com>
2013-02-18 16:06:00 -0500

    grid image and player image loaded

--- JS/gameloop.js
+++ JS/gameloop.js
@@ -17,22 +17,24 @@ function RenderUI(){
 
 	// load the images in parallel. When all the images are
 	// ready, the callback function is called.
-	scene.loadImages(['images/thief.GIF'], function() {
+	scene.loadImages(['images/thief.GIF','images/grid.jpg'], function() {
+		var mesh = scene.Sprite('images/grid.jpg');
+		mesh.update();
 	    // create the Sprite object;
 	    var sp = scene.Sprite('images/thief.GIF');
 	    // change the visible size of the sprite
-	    sp.size(175,175);
+	    //sp.size(175,175);
 	    // apply the latest visual changes to the sprite
 	    // (draw if canvas, update attribute if DOM);
 	    sp.update();
 	    // change the offset of the image in the sprite
 	    // (this works the opposite way of a CSS background)
-	    sp.offset(0, 0);
+	    //sp.offset(0, 0);
 	    // various transformations
-	    sp.move(150, 300);
+	    //sp.move(150, 300);
 	    //sp.rotate(3.14 / 4);
 	    //sp.scale(2);
-	    sp.setOpacity(0.8);
+	    //sp.setOpacity(0.8);
 
 	    sp.update();
 	});
