831cb0551c9a8a6fedee6dba9a06f77d2f5e41e6
Dhanesh Neelamana <dhanesh.n.m19@gmail.com>
2013-02-18 14:50:57 -0500

    playing with sample code/load an image

--- JS/gameloop.js
+++ JS/gameloop.js
@@ -14,8 +14,29 @@ var gameloop = function gameloop() {
 }
 function RenderUI(){
 	var scene = sjs.Scene({w:640, h:480});
-	var foreground = scene.Layer("foreground");
-	var player = scene.Sprite("images/thief.GIF", foreground);
+
+	// load the images in parallel. When all the images are
+	// ready, the callback function is called.
+	scene.loadImages(['images/thief.GIF'], function() {
+	    // create the Sprite object;
+	    var sp = scene.Sprite('images/thief.GIF');
+	    // change the visible size of the sprite
+	    sp.size(175,175);
+	    // apply the latest visual changes to the sprite
+	    // (draw if canvas, update attribute if DOM);
+	    sp.update();
+	    // change the offset of the image in the sprite
+	    // (this works the opposite way of a CSS background)
+	    sp.offset(0, 0);
+	    // various transformations
+	    sp.move(150, 300);
+	    //sp.rotate(3.14 / 4);
+	    //sp.scale(2);
+	    sp.setOpacity(0.8);
+
+	    sp.update();
+	});
+
 
 }
 gameloop();
