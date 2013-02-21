7a4dcfb4e46c54c948716f2c9aed24d70b7cdd8f
Dhanesh Neelamana <dhanesh.n.m19@gmail.com>
2013-02-21 00:41:32 -0500

    begin mesh creation logic

--- JS/gameloop.js
+++ JS/gameloop.js
@@ -17,11 +17,16 @@ function RenderUI(){
 
 	// load the images in parallel. When all the images are
 	// ready, the callback function is called.
-	scene.loadImages(['images/thief.GIF','images/grid.jpg'], function() {
+	scene.loadImages(['images/thief.GIF','images/Rocks.png'], function() {
 		var meshlayer = scene.Layer("background");
-		var mesh = scene.Sprite(false, {layer:meshlayer, color:"#c0c0c0"});
-		mesh.size(500,500);
-		mesh.update();
+		var mesh1 = scene.Sprite('images/Rocks.png', {layer:meshlayer, color:"#c0c0c0"});
+		mesh1.size(50,50);
+		mesh1.update();
+		var mesh2 = scene.Sprite('images/Rocks.png', {layer:meshlayer, color:"#c0c0c0"});
+		var unitblock = 50;
+	    mesh2.move(unitblock*2,0);
+		mesh2.size(50,50);
+		mesh2.update();
 	    // create the Sprite object;
 	    var playerLayer = scene.Layer("forground");
 	    var sp = scene.Sprite('images/thief.GIF',playerLayer);
@@ -32,8 +37,7 @@ function RenderUI(){
 	    // change the offset of the image in the sprite
 	    // (this works the opposite way of a CSS background)
 	    //sp.offset(0, 0);
-	    // various transformations
-	    var unitblock = 50;
+	    // various transformations	    
 	    sp.move(unitblock*6,0);
 	    //sp.rotate(3.14 / 4);
 	    //sp.scale(2);
